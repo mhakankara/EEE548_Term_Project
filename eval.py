9a53d2a08dc40cb883d3d7ae51a0cbe6561e8674
AbdallahKelani <45761780+AbdallahKelani@users.noreply.github.com>
2023-12-20 16:35:14 +0300

    Add files via upload

--- eval.py
+++ eval.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Dec 10 01:00:13 2023
+Created on Mon Dec 18 20:49:08 2023
+
 
 @author: Abdallah
 
@@ -23,19 +24,10 @@ import torch
 import random
 import numpy as np
 from tqdm import tqdm
-import matplotlib.pyplot as plt
-
-import matplotlib
-matplotlib.use('Agg')
 
 from torch.nn import Module, Sequential, Conv2d, ReLU, Flatten, Linear, init
-from torch.nn.functional import huber_loss
-from torch.nn.utils import clip_grad_norm_
 
 from stable_baselines3.common.atari_wrappers import MaxAndSkipEnv
-from stable_baselines3.common.buffers import ReplayBuffer
-
-os.chdir('/auto/data2/alkilani/rl_2023')
 
 
 class DQN(Module):
@@ -65,53 +57,25 @@ class DQN(Module):
         return self.network(x / 255.)
 
 
-def DQL(env, env_name,
-        N=1_000_000, n_epochs=5_000_000,
-        update_f=4, bs=32,
-        gamma=0.99, replay_start_size=50_000,
-        initial_explore=1., final_explore=0.1,
-        explore_steps=1_000_000, device='cuda',
-        seed=0, savepath='', lr_scheduler=False, clip_grads=False) -> None:
-    """ DQL - Deep Q-Learning """
+def Eval(env, env_name,
+         weights_path='', n_steps=10_000,
+         bs=32, gamma=0.99, epsilon=0.05, seed=0,
+         device='cuda', savepath='Eval_Plots') -> None:
+    """ Evaluate DQN """
     
-    # Initialize replay memory D to capacity N
-    D = ReplayBuffer(N, env.observation_space, env.action_space, device,
-                      optimize_memory_usage=True, handle_timeout_termination=False)
-
     # Initialize action-value function Q with random weights
     q_net = DQN(env.action_space.n).to(device)
-    
-    if lr_scheduler:
-        optim = torch.optim.RMSprop(
-            q_net.parameters(),
-            lr=0.01, alpha=0.95, eps=0.01, momentum=0.95
-        )
-
-        scheduler = torch.optim.lr_scheduler.ExponentialLR(optim, gamma=0.99999)
-        # scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optim, T_0=25_000, eta_min=0.00025)
-    else:
-        optim = torch.optim.RMSprop(
-            q_net.parameters(),
-            lr=0.00025, alpha=0.95, eps=0.01, momentum=0.95
-        )
-    
-    # Gradient clipping
-    if clip_grads:
-        max_grad_norm = 1.0
+    q_net.load_state_dict(torch.load(weights_path))
     
     epoch = 0
     
-    smoothed_rewards = []
     rewards_list = []
     
-    smoothed_q_values = []
     average_q_values = []
     
-    progress_bar = tqdm(total=n_epochs)
+    progress_bar = tqdm(total=n_steps)
     
-    scheduler_str = '_lr_scheduler' if lr_scheduler else ''
-    
-    while (epoch <= n_epochs):
+    while (epoch <= n_steps):
 
         died = False
         
@@ -126,106 +90,31 @@ def DQL(env, env_name,
             seq, _, _, info = env.step(1)
         
         # Play until death
-        while not died:
+        while not died and (epoch <= n_steps):
             current_life = info['lives']
-            
-            # Probability ε, decays with each epoch
-            epsilon = max(
-                (final_explore - initial_explore) / explore_steps * epoch + initial_explore,
-                final_explore
-                )
-            
+                        
             # With probability ε select a random action a.
+            q_values = q_net(torch.Tensor(seq).unsqueeze(0).to(device))
             if (random.random() < epsilon):
                 action = np.array(env.action_space.sample())
             # Otherwise select a = max_a Q^{*}(φ(s_t), a; θ)
             else:
-                q_values = q_net(torch.Tensor(seq).unsqueeze(0).to(device))
                 action = torch.argmax(q_values, dim=1).item()
+
+            # Calculate and append average Q value
+            max_q_values.append(q_values.max().item())
 
             # Execute action a in emulator and observe reward r_t and image x_{t+1}
             next_seq, r, died, info = env.step(action)
 
-            died = (info['lives'] < current_life) or died
-
-            # Set s_{t+1} = s_t, a_t, x_{t+1} and preprocess φ_{t+1} = φ(s_{t+1})
-            real_next_seq = next_seq.copy()
+            died = True if (info['lives'] < current_life) else False
 
             total_rewards += r
-            # Reward clipping
-            r = np.sign(r)
             
-            # Store transition (φ_t, a_t, r_t, φ_{t+1}) in D
-            D.add(seq, real_next_seq, action, r, died, info)
-
             seq = next_seq
             
-            # Minibatch updates
-            if (epoch >= replay_start_size) and (epoch % update_f == 0):
-                # Sample random minibatch of transitions (φ_j , a_j , r_j , φ_{j +1} ) from D
-                minibatch = D.sample(bs)
-                with torch.no_grad():
-                    max_q_value, _ = q_net(minibatch.next_observations).max(dim=1)
-                    y = minibatch.rewards.flatten() + gamma * max_q_value * (1 - minibatch.dones.flatten())
-                current_q_value = q_net(minibatch.observations).gather(1, minibatch.actions).squeeze()
-                loss = huber_loss(y, current_q_value)
-                
-                # Calculate and append average Q value
-                max_q_values.append(current_q_value.max().item())
-
-                # Perform a gradient step according to equation 3 of the paper
-                optim.zero_grad()
-                # Calculate gradients and perform backward pass
-                loss.backward()
-                if clip_grads:
-                    # Clip gradients to the specified range [-max_grad_norm, max_grad_norm]
-                    clip_grad_norm_(q_net.parameters(), max_grad_norm)
-                optim.step()
-                                
-                # Update scheduler
-                if lr_scheduler and scheduler.get_last_lr()[-1] > 0.00025:
-                    scheduler.step()
-                
-                # # Update scheduler
-                # if lr_scheduler:
-                #     scheduler.step(epoch - replay_start_size)
-
             epoch += 1
             
-            # Plot and save
-            if (epoch % 50_000 == 0) and (epoch > 0):
-                smoothed_rewards.append(np.mean(rewards_list))
-                rewards_list = []
-                
-                plt.figure()
-                plt.plot(smoothed_rewards)
-                plt.title(f'Average Reward on {env_name.title()}')
-                plt.xlabel('Training Epochs')
-                plt.ylabel('Average Reward per Episode')
-                plt.savefig(fr'{savepath}/{env_name.title()}_reward_{seed}{scheduler_str}.png')
-                plt.close()
-                
-                smoothed_q_values.append(np.mean(average_q_values))
-                average_q_values = []
-                
-                plt.figure()
-                plt.plot(smoothed_q_values)
-                plt.title(f'Average Q on {env_name.title()}')
-                plt.xlabel('Training Epochs')
-                plt.ylabel('Average Action Value (Q)')
-                plt.savefig(fr'{savepath}/{env_name.title()}_Q_{seed}{scheduler_str}.png')
-                plt.close()
-                
-                with open(fr'{savepath}/{env_name.title()}_reward_{seed}{scheduler_str}.npy', 'wb') as f:
-                    np.save(f, np.array(smoothed_rewards))
-                
-                with open(fr'{savepath}/{env_name.title()}_Q_{seed}{scheduler_str}.npy', 'wb') as f:
-                    np.save(f, np.array(smoothed_q_values))
-                    
-                # Save model
-                # https://pytorch.org/tutorials/beginner/saving_loading_models.html#save-load-state-dict-recommended
-                torch.save(q_net.state_dict(), fr'{savepath}/{env_name.title()}_{seed}{scheduler_str}.pth')
-                
             progress_bar.update()
         
         # Append results if not NaN/None/[]
@@ -238,6 +127,18 @@ def DQL(env, env_name,
             average_q_values.append(0.)
         else:
             average_q_values.append(np.mean(max_q_values))
+            
+    with open(fr'{savepath}\{env_name.title()}_reward_{seed}_EVAL.npy', 'wb') as f:
+        np.save(f, np.array(rewards_list))
+    
+    with open(fr'{savepath}\{env_name.title()}_Q_{seed}_EVAL.npy', 'wb') as f:
+        np.save(f, np.array(average_q_values))
+    
+    with open(fr'{savepath}\summary.txt', 'a') as f:
+        f.write(f'\n{env_name.title()} seed={seed} averages for evaluation: \tReward = {np.mean(rewards_list):.10} \tQ = {np.mean(average_q_values):.10}\n')
+        
+    return
+
 
 class AtariCropWrapper(gym.Wrapper):
     def __init__(self, env, game_name):
@@ -282,13 +183,14 @@ class AtariCropWrapper(gym.Wrapper):
         return self.preprocess_observation(observation)
 
 
-def create_env(env_name, k=4, seed=0) -> gym.Env:
+def create_env(env_name, k=4, seed=0, crop=False) -> gym.Env:
     # env = gym.make(f'ALE/{env_name}-v5')
     env = gym.make(f'{env_name}NoFrameskip-v4') # Better behavior
     # env = gym.make(f'{env_name}NoFrameskip-v4', render_mode='human') # Better behavior
 
     # Replicate paper's conditions (grayscale, frameskip, etc.)
-    env = AtariCropWrapper(env, env_name)
+    if crop:
+        env = AtariCropWrapper(env, env_name)
     env = gym.wrappers.RecordEpisodeStatistics(env)
     env = gym.wrappers.ResizeObservation(env, (84, 84))
     env = gym.wrappers.GrayScaleObservation(env)
@@ -310,15 +212,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--seed', default=0, type=int)                  
     parser.add_argument('--env', default=env_list[0], choices=env_list)
-    parser.add_argument('--savepath', default='Env_Plots')
+    parser.add_argument('--savepath', default='')
+    parser.add_argument('--weights', default='')
     parser.add_argument('--device', default='cuda')
-    parser.add_argument('--epochs', default=5_000_000, type=int)
-    parser.add_argument('--N', default=750_00, type=int)              
-    parser.add_argument('--lr_scheduler', action='store_true')
-    parser.add_argument('--clipgrads', action='store_true')
+    parser.add_argument('--steps', default=10_000, type=int)
+    parser.add_argument('--epsilon', default=0.05, type=float)
+    parser.add_argument('--cropenv', action='store_true')
     args = parser.parse_args()     
     
-    savepath = os.path.join(os.getcwd(), args.savepath)
+    savepath = os.path.join(os.getcwd(), args.savepath, 'EVAL')
+    weights = os.path.join(os.getcwd(), *args.weights.split('/'))
     
     # Create folder for saving plots and .npy files, if doesn't already exist
     if not os.path.isdir(savepath):
@@ -334,15 +237,15 @@ if __name__ == '__main__':
     # Create env
     # k = 3 if args.env == 'SpaceInvaders' else 4 # Lasers disappear, apparently
     k = 4
-    env = create_env(args.env, k=k, seed=args.seed)
+    env = create_env(args.env, k=k, seed=args.seed, crop=args.cropenv)
     
     print('Environment:', args.env)
     print('Device:', device)
     
     # Maximum N allowed due to mem limits === 750_000 (<1_000_000)
-    DQL(env, args.env, n_epochs=args.epochs, N=args.N, update_f=k, device=device, seed=args.seed, savepath=savepath, lr_scheduler=args.lr_scheduler, clip_grads=args.clipgrads)
+    Eval(env, args.env, 
+         weights_path=weights, n_steps=args.steps,
+         seed=args.seed, savepath=savepath,
+         device=device, epsilon=args.epsilon)
     
-    env.close()
-            
-
-
+    env.close()    
---
+++ eval_random.py
@@ -0,0 +1,193 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 18 20:49:08 2023
+
+
+@author: Abdallah
+
+Implementation/Recreation attempt of the seminal paper:
+- "Playing Atari with Deep Reinforcement Learning"
+- (Main) Paper: https://arxiv.org/pdf/1312.5602.pdf
+- Also Helpful: https://doi.org/10.1038/nature14236
+
+The implementation is Torch-based and relies on OpenAI's Gymnasium for ATARI game simulation.
+- https://pytorch.org/get-started/previous-versions/#v1101
+
+Stables Baselines 3's ReplayBuffer and MaxAndSkipEnv are used for generalizability/reproducibility.
+- http://jmlr.org/papers/v22/20-1364.html
+"""
+
+import os
+import argparse
+import gym
+import random
+import numpy as np
+from tqdm import tqdm
+
+
+from stable_baselines3.common.atari_wrappers import MaxAndSkipEnv
+
+
+def Eval(env, env_name,
+         weights_path='', n_steps=10_000,
+         bs=32, gamma=0.99, epsilon=0.05, seed=0,
+         device='cuda', savepath='Eval_Plots') -> None:
+    """ Evaluate DQN """
+    
+    epoch = 0
+    
+    rewards_list = []
+    
+    average_q_values = []
+    
+    progress_bar = tqdm(total=n_steps)
+    
+    while (epoch <= n_steps):
+
+        died = False
+        
+        total_rewards = 0
+        max_q_values = []
+
+        # Initialise sequence s_1 = \{x_1\} and preprocessed sequenced φ_1 = φ(s_1)
+        seq = env.reset()
+
+        # Fire after reset / "do nothing"
+        for _ in range(random.randint(1, 30)):
+            seq, _, _, info = env.step(1)
+        
+        # Play until death
+        while not died and (epoch <= n_steps):
+            current_life = info['lives']
+            
+            action = np.array(env.action_space.sample())
+
+            # Execute action a in emulator and observe reward r_t and image x_{t+1}
+            next_seq, r, died, info = env.step(action)
+
+            died = True if (info['lives'] < current_life) else False
+
+            total_rewards += r
+            
+            seq = next_seq
+            
+            epoch += 1
+            
+            progress_bar.update()
+        
+        # Append results if not NaN/None/[]
+        if not total_rewards:
+            rewards_list.append(0.)
+        else:
+            rewards_list.append(total_rewards)
+            
+    with open(fr'{savepath}\{env_name.title()}_reward_{seed}_EVAL.npy', 'wb') as f:
+        np.save(f, np.array(rewards_list))
+    
+    with open(fr'{savepath}\summary.txt', 'a') as f:
+        f.write(f'\n{env_name.title()} seed={seed} averages for evaluation: \tReward = {np.mean(rewards_list):.10}\n')
+        
+    return
+
+
+class AtariCropWrapper(gym.Wrapper):
+    def __init__(self, env, game_name):
+        super(AtariCropWrapper, self).__init__(env)
+        self.game_name = game_name
+        self.crop_values = self.get_crop_values(game_name)
+        
+        # Determine the dimensions after cropping
+        self.crop_height = self.crop_values[2] - self.crop_values[0]
+        self.crop_width = self.crop_values[3] - self.crop_values[1]
+        
+        # Set the observation space to the cropped dimensions
+        self.observation_space = gym.spaces.Box(low=0, high=255, shape=(self.crop_height, self.crop_width, 3), dtype=np.uint8)
+
+    def get_crop_values(self, game_name):
+        # Define crop values for different Atari games
+        crop_values = {
+            'BeamRider': (30, 5, 195, 160),
+            'Breakout': (30, 5, 195, 160),
+            'Enduro': (30, 10, 195, 150),
+            'Pong': (35, 5, 190, 160),
+            'Qbert': (30, 5, 190, 160),
+            'Seaquest': (30, 10, 190, 160),
+            'SpaceInvaders': (30, 5, 190, 160),
+        }
+        return crop_values.get(game_name, (0, 0, 210, 160))  # Default to (0, 0, 210, 160) if game_name not found
+
+    def preprocess_observation(self, observation):
+        # Crop observation without converting to grayscale
+        cropped = observation[self.crop_values[0]:self.crop_values[2], self.crop_values[1]:self.crop_values[3], :]
+        return cropped
+    
+    def step(self, action):
+        observation, reward, done, info = self.env.step(action)
+        return self.preprocess_observation(observation), reward, done, info
+
+    def reset(self, seed=None, options=None):
+        # We need the following line to seed self.np_random
+        super().reset(seed=seed)
+        
+        observation = self.env.reset()
+        return self.preprocess_observation(observation)
+
+
+def create_env(env_name, k=4, seed=0, crop=False) -> gym.Env:
+    # env = gym.make(f'ALE/{env_name}-v5')
+    env = gym.make(f'{env_name}NoFrameskip-v4') # Better behavior
+    # env = gym.make(f'{env_name}NoFrameskip-v4', render_mode='human') # Better behavior
+
+    # Replicate paper's conditions (grayscale, frameskip, etc.)
+    if crop:
+        env = AtariCropWrapper(env, env_name)
+    env = gym.wrappers.RecordEpisodeStatistics(env)
+    env = gym.wrappers.ResizeObservation(env, (84, 84))
+    env = gym.wrappers.GrayScaleObservation(env)
+    env = gym.wrappers.FrameStack(env, k)
+    env = MaxAndSkipEnv(env, skip=k)
+    
+    # Fix initial seed
+    env.seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    
+    return env
+
+if __name__ == '__main__':
+    # Environments considered in the paper
+    env_list = ['BeamRider', 'Breakout', 'Enduro', 'Pong', 'Qbert', 'Seaquest', 'SpaceInvaders']
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', default=0, type=int)                  
+    parser.add_argument('--env', default=env_list[0], choices=env_list)
+    parser.add_argument('--savepath', default='')
+    parser.add_argument('--steps', default=10_000, type=int)
+    parser.add_argument('--epsilon', default=1.0, type=float)
+    parser.add_argument('--cropenv', action='store_true')
+    args = parser.parse_args()     
+    
+    savepath = os.path.join(os.getcwd(), args.savepath, 'EVAL')
+    weights = os.path.join(os.getcwd(), *args.weights.split('/'))
+    
+    # Create folder for saving plots and .npy files, if doesn't already exist
+    if not os.path.isdir(savepath):
+        os.makedirs(savepath)
+    
+    # Check if CUDA is available
+    device = 'cpu'
+    
+    # Create env
+    # k = 3 if args.env == 'SpaceInvaders' else 4 # Lasers disappear, apparently
+    k = 4
+    env = create_env(args.env, k=k, seed=args.seed, crop=args.cropenv)
+    
+    print('Environment:', args.env)
+    print('Device:', device)
+    
+    # Maximum N allowed due to mem limits === 750_000 (<1_000_000)
+    Eval(env, args.env, n_steps=args.steps,
+         seed=args.seed, savepath=savepath, epsilon=args.epsilon)
+    
+    env.close()    
